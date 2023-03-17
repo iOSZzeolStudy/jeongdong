5243bb11a54f3e2fd37f78d70f9e56c489fa9950
jeongdung-eo <jeuniis2@gmail.com>
2023-03-17 13:54:12 +0900

    Initial Commit

---
+++ AZzeolMemo/AZzeolMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AZzeolMemoApp.swift
+//  AZzeolMemo
+//
+//  Created by JEONGEUN KIM on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct AZzeolMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
